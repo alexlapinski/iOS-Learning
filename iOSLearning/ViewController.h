268e054f21643dddd7d2e53285ca2f633a4b1e10
Alex Lapinski <alexander.lapinski@razorfishhealth.com>
2012-09-07 08:19:54 -0400

    Initial Commit

---
+++ iOSLearning/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iOSLearning
+//
+//  Created by Alexander Lapinski on 9/7/12.
+//  Copyright (c) 2012 Alex Lapinski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iOSLearning/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  iOSLearning
+//
+//  Created by Alexander Lapinski on 9/7/12.
+//  Copyright (c) 2012 Alex Lapinski. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES;
+}
+
+@end
